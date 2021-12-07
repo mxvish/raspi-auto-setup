cdc87c1a1fdd2f715d1421d2bbdac972d1abb982
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2021-12-07 20:14:58 +0900

    Add files via upload

---
+++ raspisetup.sh
@@ -0,0 +1,45 @@
+#install raspberry pi OS Lite on micro SD using raspberry pi imager 
+#connect raspberry pi to wired LAN & display
+#turn on raspberry pi
+#username = pi
+#password = raspberry
+
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y install raspberrypi-ui-mods
+
+#after GUI was installed
+sudo apt -y install mpg321
+sudo apt -y install fcitx-mozc
+sudo apt -y install python3-pip 
+
+#to install i2cdetect
+sudo apt -y install i2c-tools
+
+sudo apt -y install python3-tk 
+
+#install gpio command
+sudo apt -y install git
+git clone https://github.com/wiringpi/wiringpi
+cd wiringpi
+./build
+#gpio -v
+#gpio readall
+
+sudo apt -y install mu-editor 
+sudo apt -y install tree 
+pip3 install wiringpi
+
+#midori=web browser with ad blocker
+sudo apt -y install midori
+
+#pip3 install jaconv
+#installed python3 modules:
+	#tkinter, wiringpi, random, time, subprocess, RPi.GPIO, unicodedata 
+
+#not installed: pysimplegui, jaconv
+
+#some commands are aliased at ~/.bashrc
+
+#reference
+#https://youtu.be/y45hsdAOpw
