c9a8a659b585702f7a095f5adcd1f121a81cd277
Kenta Oshima <84490675+kenteroshima@users.noreply.github.com>
2022-05-03 15:55:04 +0900

    Update raspisetup.sh

--- raspisetup.sh
+++ raspisetup.sh
@@ -50,7 +50,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 mv vimrc .vimrc
 #sudo apt -y install agnostics
 sudo apt -y install nodejs
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 reboot
