3babf6e9de82504550fd2c8ab61fd8fc878c0cd4
Kenta Oshima <84490675+mxvish@users.noreply.github.com>
2025-03-19 00:22:22 +0900

    run startx by default

--- setup.sh
+++ setup.sh
@@ -30,6 +30,10 @@ DEFAULT_USER=mxvish
 echo 'exec i3' > /home/$DEFAULT_USER/.xinitrc
 curl https://raw.githubusercontent.com/mxvish/vimrc/main/vimrc > /home/$DEFAULT_USER/.vimrc
 
+echo -e 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then
+\texec startx
+fi' >> /home/$DEFAULT_USER/.bash_profile
+
 curl https://raw.githubusercontent.com/mxvish/i3config/main/config > ~/.config/i3/config
 wget https://raw.githubusercontent.com/mxvish/i3status/main/i3status.conf
 sudo mv i3status.conf /etc/
